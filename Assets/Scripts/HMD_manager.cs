67f5abab1671f54128324b8e704558852b35530a
Vale1505 <165145001+Vale1505@users.noreply.github.com>
2026-02-23 10:19:47 +0000

    update

---
+++ Assets/Scripts/HMD_manager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMD_manager : MonoBehaviour
+{
+    [SerializeField] GameObject xrPlayer;
+    [SerializeField] GameObject fpsPlayer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      Debug.Log("Using device:"+ XRSettings.loadedDeviceName);  
+      if (XRSettings.isDeviceActive || XRSettings.loadedDeviceName == "OpenXR Display")
+        {
+            Debug.Log("Using device XR Player with HMD : " + XRSettings.loadedDeviceName);
+            fpsPlayer.SetActive(false);
+            xrPlayer.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("No HMD detected, using FPS player");
+            fpsPlayer.SetActive(true);
+            xrPlayer.SetActive(false);
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
