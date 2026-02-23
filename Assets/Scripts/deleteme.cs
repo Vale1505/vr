2ca28f8979a98fbfef8936e192b6b441f594e41b
Vale1505 <165145001+Vale1505@users.noreply.github.com>
2026-02-23 11:08:04 +0000

    1

--- Assets/Scripts/deleteme.cs
+++ Assets/Scripts/deleteme.cs
@@ -5,7 +5,7 @@ public class deleteme : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // ignor and delte
     }
 
     // Update is called once per frame
